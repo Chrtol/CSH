351dc343860b32e17fb0364dead5105ec7e9f4ec
chrtol <chrtol95@gmail.com>
2015-06-22 03:01:33 +0200

    test from intellij

--- ChampSelectHelper/src/main/SummonerData.java
+++ ChampSelectHelper/src/main/SummonerData.java
@@ -1,7 +1,17 @@
 package main;
 
-/**
- * Created by ChristianFredrik on 22.06.2015.
- */
+import java.util.Map;
+
+import constant.Region;
+import dto.Summoner.Summoner;
+import main.java.riotapi.RiotApi;
+
 public class SummonerData {
+
+    SummonerData () {
+
+    }
+
+    RiotApi api = new RiotApi("d62a7df2-cbc6-4e6e-b964-8cf42b4c0a71");
+
 }
